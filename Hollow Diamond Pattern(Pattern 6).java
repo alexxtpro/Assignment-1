5c19b22ad41f4d949b14c17d791bb9dbbe11c839
Ajay Virmoti <73848796+Ajayvirmoti@users.noreply.github.com>
2022-08-21 18:10:58 +0530

    Create Hollow Diamond Pattern(Pattern 6).java
    Take N (number of rows), print the following pattern .

---
+++ Hollow Diamond Pattern(Pattern 6).java
@@ -0,0 +1,30 @@
+/*
+     * * * * *
+     * *   * *
+     *       *
+     * *   * *
+     * * * * *
+*/
+
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N;j++){
+                if(i==1 || j==N || i==N ||j==1 || (i<=(N/2+1)&&j<=(N/2+2)-i) || (i<=N/2&&j>((N/2)-1)+i)){
+                    System.out.print("*	");
+                }else if(((i>(N/2)+1)&&j<=(N/2+1)-(N-i))||(i>N/2+1&&j>((N/2))+(N-i))){
+                    System.out.print("*	");
+                }else{
+                    System.out.print("	");
+                }
+
+            }
+            System.out.println();
+
+        }
+    }
+}
